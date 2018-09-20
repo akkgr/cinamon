d38dba804660aae8988c54105d15aba74d189e2a
akkgr <akkgr@users.noreply.github.com>
2018-09-20 20:20:04 +0300

    fix

--- main.go
+++ main.go
@@ -1,34 +1,34 @@
 package main
 
 import (
-	"context"
+	"crypto/tls"
+	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/render"
-	"github.com/pressly/lg"
 	"github.com/sirupsen/logrus"
+)
+
+var (
+	// CertFile ... os.Getenv("GCUK_CERT_FILE")
+	CertFile = "./certs/local.localhost.cert"
+	// KeyFile ...
+	KeyFile = "./certs/local.localhost.key"
+	// ServiceAddr ...
+	ServiceAddr = ":8080"
 )
 
 func main() {
 	logger := logrus.New()
 	logger.Formatter = &logrus.JSONFormatter{}
 
-	lg.RedirectStdlogOutput(logger)
-	lg.DefaultLogger = logger
-
-	lg.Infoln("Welcome")
-
-	serverCtx := context.Background()
-	serverCtx = lg.WithLoggerContext(serverCtx, logger)
-	lg.Log(serverCtx).Infof("Booting up server, %s", "v1.0")
-
 	r := chi.NewRouter()
-	r.Use(lg.RequestLogger(logger))
 	r.Use(middleware.CloseNotify)
 	r.Use(middleware.RequestID)
+	r.Use(NewStructuredLogger(logger))
 	r.Use(middleware.RealIP)
 
 	r.Use(middleware.Recoverer)
@@ -41,6 +41,132 @@ func main() {
 		w.Write([]byte("hi"))
 	})
 
-	service := chi.ServerBaseContext(serverCtx, r)
-	http.ListenAndServe(":5000", service)
+	// service := chi.ServerBaseContext(serverCtx, r)
+	// srv := serve(service, ServiceAddr)
+	// err := srv.ListenAndServeTLS(CertFile, KeyFile)
+	err := http.ListenAndServe(ServiceAddr, r)
+	if err != nil {
+		logger.Fatalf("server failed to start: %v", err)
+	}
+}
+
+func serve(handler http.Handler, serverAddress string) *http.Server {
+	// See https://blog.cloudflare.com/exposing-go-on-the-internet/ for details
+	// about these settings
+	tlsConfig := &tls.Config{
+		// Causes servers to use Go's default cipher suite preferences,
+		// which are tuned to avoid attacks. Does nothing on clients.
+		PreferServerCipherSuites: true,
+		// Only use curves which have assembly implementations
+		CurvePreferences: []tls.CurveID{
+			tls.CurveP256,
+			tls.X25519, // Go 1.8 only
+		},
+
+		MinVersion: tls.VersionTLS12,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+		},
+	}
+	srv := &http.Server{
+		Addr:         serverAddress,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+		TLSConfig:    tlsConfig,
+		Handler:      handler,
+	}
+	return srv
+}
+
+// StructuredLogger is a simple, but powerful implementation of a custom structured
+// logger backed on logrus. I encourage users to copy it, adapt it and make it their
+// own. Also take a look at https://github.com/pressly/lg for a dedicated pkg based
+// on this work, designed for context-based http routers.
+
+func NewStructuredLogger(logger *logrus.Logger) func(next http.Handler) http.Handler {
+	return middleware.RequestLogger(&StructuredLogger{logger})
+}
+
+type StructuredLogger struct {
+	Logger *logrus.Logger
+}
+
+func (l *StructuredLogger) NewLogEntry(r *http.Request) middleware.LogEntry {
+	entry := &StructuredLoggerEntry{Logger: logrus.NewEntry(l.Logger)}
+	logFields := logrus.Fields{}
+
+	logFields["ts"] = time.Now().UTC().Format(time.RFC1123)
+
+	if reqID := middleware.GetReqID(r.Context()); reqID != "" {
+		logFields["req_id"] = reqID
+	}
+
+	scheme := "http"
+	if r.TLS != nil {
+		scheme = "https"
+	}
+	logFields["http_scheme"] = scheme
+	logFields["http_proto"] = r.Proto
+	logFields["http_method"] = r.Method
+
+	logFields["remote_addr"] = r.RemoteAddr
+	logFields["user_agent"] = r.UserAgent()
+
+	logFields["uri"] = fmt.Sprintf("%s://%s%s", scheme, r.Host, r.RequestURI)
+
+	entry.Logger = entry.Logger.WithFields(logFields)
+
+	entry.Logger.Infoln("request started")
+
+	return entry
+}
+
+type StructuredLoggerEntry struct {
+	Logger logrus.FieldLogger
+}
+
+func (l *StructuredLoggerEntry) Write(status, bytes int, elapsed time.Duration) {
+	l.Logger = l.Logger.WithFields(logrus.Fields{
+		"resp_status": status, "resp_bytes_length": bytes,
+		"resp_elapsed_ms": float64(elapsed.Nanoseconds()) / 1000000.0,
+	})
+
+	l.Logger.Infoln("request complete")
+}
+
+func (l *StructuredLoggerEntry) Panic(v interface{}, stack []byte) {
+	l.Logger = l.Logger.WithFields(logrus.Fields{
+		"stack": string(stack),
+		"panic": fmt.Sprintf("%+v", v),
+	})
+}
+
+// Helper methods used by the application to get the request-scoped
+// logger entry and set additional fields between handlers.
+//
+// This is a useful pattern to use to set state on the entry as it
+// passes through the handler chain, which at any point can be logged
+// with a call to .Print(), .Info(), etc.
+
+func GetLogEntry(r *http.Request) logrus.FieldLogger {
+	entry := middleware.GetLogEntry(r).(*StructuredLoggerEntry)
+	return entry.Logger
+}
+
+func LogEntrySetField(r *http.Request, key string, value interface{}) {
+	if entry, ok := r.Context().Value(middleware.LogEntryCtxKey).(*StructuredLoggerEntry); ok {
+		entry.Logger = entry.Logger.WithField(key, value)
+	}
+}
+
+func LogEntrySetFields(r *http.Request, fields map[string]interface{}) {
+	if entry, ok := r.Context().Value(middleware.LogEntryCtxKey).(*StructuredLoggerEntry); ok {
+		entry.Logger = entry.Logger.WithFields(fields)
+	}
 }
